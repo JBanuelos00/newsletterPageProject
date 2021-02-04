066f81b521132fd13be54fe7ed5e857ff45fd9f1
Javier Banuelos <J.Banuelos00@gmail.com>
2021-02-03 22:04:51 -0800

    Image, css, static folder, and post
    app.js was updated with the correct post method while signup.html was given the bootstrap simple form and the right adjustments to allow it to work with app.js. The static method was added so the website could pull from a static public folder, currently only has styles.css and an image.

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const request = require('request');
 
 const app = express();
 
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post('/', (req, res) => {
+  const fName = req.body.firstName;
+  const lName = req.body.lastName;
+  const email = req.body.email;
+
+  console.log(fName, lName, email);
+});
 
 app.listen(port, () => {
   console.log(`The server is running on port ${port}`);
